bc1989ce7bd62bf2703c0332ed84893bbeb13af7
Anonymous么kid <79362039+dangerbots@users.noreply.github.com>
2022-10-08 07:31:18 +0530

    Update pingallbot.py

--- pingallbot.py
+++ pingallbot.py
@@ -234,11 +234,9 @@ async def bots(client, message):
 async def start(client, message):
   text = f'''
 Heya {message.from_user.mention},
-My name is **PingAll**. I'm here to help you to get everyone's attention by mentioning all members in your chat.
+My name is **Tagpro**. I'm here to help you to get everyone's attention by mentioning all members in your chat.
 
 I have some additional cool features and also I can work in channels.
-
-Don't forget to join my [channel](http://t.me/teletipsofficialchannel) to recieve information on all the latest updates.
 
 Hit /help to find out my commands and the use of them.
 '''
@@ -254,10 +252,7 @@ Hey, let's have a quick look at my commands.
 - /ping "input": <i>Mention all members.</i>
 - /remove: <i>Remove all deleted accounts.</i>
 - /admins: <i>Mention all admins.</i>
-- /bots: <i>Get the full bot list.</i>
-- /stop: <i>Stop an on going process.</i>
-
-If you have any questions on how to use me, feel free to ask in my [support group](https://t.me/teletipsofficialontopicchat). More on my [page](https://github.com/teletips/PingAllBot-TeLeTiPs).
+- /bots: <i>Get
 '''
   await teletips.send_message(message.chat.id, text, disable_web_page_preview=True)
 
